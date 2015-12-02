23b467590b7594eee6fc42842d0f839d4c707b7b
Josh Fletcher <jfletcher@nuxeo.com>
2015-12-01 16:55:20 -0800

    Remove unnecessary imports.

--- src/main/java/org/nuxeo/sample/operation/PictureToPPTXOperation.java
+++ src/main/java/org/nuxeo/sample/operation/PictureToPPTXOperation.java
@@ -20,17 +20,11 @@ package org.nuxeo.sample.operation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.core.Constants;
-import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
-import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
-import org.nuxeo.ecm.automation.core.collectors.BlobCollector;
 import org.nuxeo.ecm.core.api.Blob;
-import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
-import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.sample.pptx4j.PictureToPPTX;
 
